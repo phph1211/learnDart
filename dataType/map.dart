62f0c110a22ee37a260897c04a0ef97f9893b3c4
박한 <bagkan1211@gmail.com>
2025-06-12 09:17:33 +0900

    map

---
+++ dataType/map.dart
@@ -0,0 +1,13 @@
+void main() {
+  var user = {
+    "name": "박한",
+    "age": 19,
+    "isStudent": true,
+  };
+
+  Map<String, Object> user2 = {
+    "name": "Parkhan",
+    "age": 19,
+    "isStudent": true,
+  };
+}
